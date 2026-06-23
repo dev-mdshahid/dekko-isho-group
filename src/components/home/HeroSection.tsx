960a1a08607c049000c38421243f3b0ddc0dd4d1
dev-mdshahid <mdshahidulridoy@gmail.com>
2026-06-23 12:31:52 +0600

    fix: scroll reveal issue in hero section

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,5 +1,4 @@
 import { ButtonArrow } from '../ui/ButtonArrow'
-import { FadeIn } from '../ui/FadeIn'
 import { PreSectionTitle } from '../ui/PreSectionTitle'
 import { NoiseOverlay, SectionLines } from '../ui/SectionDecor'
 
@@ -10,16 +9,16 @@ export function HeroSection() {
         <div className="hero-wrap">
           <div className="w-layout-grid grid-hero">
             <div className="hero-left-wrap">
-              <FadeIn id="257211a5-2a9d-1221-d387-56b813a33b41" className="hero-left-inner">
+              <div className="hero-left-inner">
                 <PreSectionTitle title="Pioneering Innovation" />
                 <div className="hero-title-wrap">
                   <h1 className="hero-title">
                     Elevating <span className="text-linear-gradient">Excellence</span> Through Innovation
                   </h1>
                 </div>
-              </FadeIn>
+              </div>
             </div>
-            <FadeIn id="89b15f62-94c9-1c8d-5eb8-ed12f4e88666" className="hero-right-wrap">
+            <div className="hero-right-wrap">
               <div className="hero-content-text">// SINCE - 1953 //</div>
               <p className="hero-description">
                 A dynamic conglomerate focused on innovation, sustainability & creating value across
@@ -28,26 +27,20 @@ export function HeroSection() {
               <div className="hero-button">
                 <ButtonArrow to="#About-Section" label="Explore our capabilities" />
               </div>
-            </FadeIn>
+            </div>
           </div>
           <div className="hero-bottom-wrap">
-            <FadeIn
-              id="7dec0c57-d1b2-b423-ef21-282893928583"
-              className="hero-bottom-text"
-            >
+            <div className="hero-bottom-text">
               Corporate HQ, Dhaka: West Tower, Level 16-19, 187, 188/B, Tejgaon Gulshan Link Road
-            </FadeIn>
-            <FadeIn id="33e8caeb-8f86-0a37-d251-e7111d4b615c">
+            </div>
+            <div>
               <a href="#Footer" className="hero-bottom-link">
                 SCROLL DOWN
               </a>
-            </FadeIn>
-            <FadeIn
-              id="22ca442b-6b4e-ae70-b69a-dc7eb35ab8aa"
-              className="hero-bottom-text"
-            >
+            </div>
+            <div className="hero-bottom-text">
               23.76994° N, 90.40694° E
-            </FadeIn>
+            </div>
           </div>
         </div>
       </div>
